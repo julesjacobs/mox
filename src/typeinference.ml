2f09ed03e94ae4ab0ee3dac0b3d16dd33bbd1dc6
julesjacobs <julesjacobs@gmail.com>
2025-11-11 02:04:57 -0500

    Update typeinference.ml

--- src/typeinference.ml
+++ src/typeinference.ml
@@ -99,6 +99,10 @@ let assert_storage_leq (lower : storage_mode) (upper : storage_mode) =
   Modesolver.Uniqueness.assert_leq_in lower.uniqueness upper.uniqueness;
   Modesolver.Areality.assert_leq_in lower.areality upper.areality
 
+let assert_future_leq lower upper =
+  if not (Modes.Future.leq_in lower upper) then
+    invalid_arg "solve_with_arrow: future mode cannot be satisfied"
+
 let rec solve_with_pair meta =
   match meta.solution with
   | Some (TyPair (TyMeta left_meta, storage, TyMeta right_meta)) ->
@@ -164,3 +168,37 @@ let rec solve_with_sum meta =
       List.iter handle_lower lower_bounds;
       List.iter handle_upper upper_bounds;
       (left_meta, storage_mode, right_meta)
+
+let component_modes_arrow modes = failwith "TODO"
+
+let rec solve_with_arrow meta =
+  match meta.solution with
+  | Some (TyArrow (TyMeta domain_meta, arrow_mode, TyMeta codomain_meta)) ->
+      (domain_meta, arrow_mode, codomain_meta)
+  | Some (TyArrow _) ->
+      invalid_arg "solve_with_arrow: expected meta components"
+  | Some _ ->
+      invalid_arg "solve_with_arrow: meta already solved to non-arrow type"
+  | None ->
+      let component_modes, arrow_mode = component_modes_arrow meta.modes in
+      let domain_meta = fresh_meta ~modes:component_modes () in
+      let codomain_meta = fresh_meta ~modes:component_modes () in
+      meta.solution <- Some (TyArrow (TyMeta domain_meta, arrow_mode, TyMeta codomain_meta));
+      let lower_bounds = meta.lower_bounds in
+      let upper_bounds = meta.upper_bounds in
+      meta.lower_bounds <- [];
+      meta.upper_bounds <- [];
+      let handle_lower bound =
+        let (lower_domain, lower_future, lower_codomain) = solve_with_arrow bound in
+        assert_subtype domain_meta lower_domain;
+        assert_subtype lower_codomain codomain_meta;
+        assert_future_leq lower_future arrow_mode
+      and handle_upper bound =
+        let (upper_domain, upper_future, upper_codomain) = solve_with_arrow bound in
+        assert_subtype upper_domain domain_meta;
+        assert_subtype codomain_meta upper_codomain;
+        assert_future_leq arrow_mode upper_future
+      in
+      List.iter handle_lower lower_bounds;
+      List.iter handle_upper upper_bounds;
+      (domain_meta, arrow_mode, codomain_meta)
